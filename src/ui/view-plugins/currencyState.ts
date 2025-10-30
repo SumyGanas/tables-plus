b9c0730686c0c3fbd0a794d4bcc63f8d603bca93
Sumy Ganas <aksmy.997@gmail.com>
2025-10-30 10:47:52 -0700

    initial commit

---
+++ src/ui/view-plugins/currencyState.ts
@@ -0,0 +1,43 @@
+
+import { StateEffect, StateField, EditorState, Transaction} from "@codemirror/state"
+import { EditorView } from '@codemirror/view';
+
+export const currencyEffects = StateEffect.define<Set<string>>();
+export const currencyField = StateField.define<Set<string>>({
+    create(state: EditorState): Set<string> {
+      return currencySet;
+    },
+    update(val: Set<string>, transaction: Transaction): Set<string> {
+      let newState = val;
+  
+      for (let effect of transaction.effects) {
+        if (effect.is(currencyEffects)) {
+          newState = effect.value;
+        }
+      }
+      return newState;
+    },
+  });
+export function addCurrencies(view: EditorView, currencies: Set<string>) {
+    view.dispatch({
+      effects: [currencyEffects.of(currencies)],
+    });
+  }
+
+export const currencies = ["$","€","£","¥","₩","₹","₱"]
+export const currencySet = new Set<string>();
+currencies.forEach((c)=>{
+    currencySet.add(c)
+})
+
+export const currencyTypes: Record<string, string> = {
+    "-":"",
+    "Add your own":"Add your own",
+    "$":"$",
+    "€":"€",
+    "£":"£",
+    "¥":"¥",
+    "₩":"₩",
+    "₹":"₹",
+    "₱":"₱"
+}
