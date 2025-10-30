a3a67724cdf07716e3b10711b6e64f49d774bc84
Sumy Ganas <aksmy.997@gmail.com>
2025-10-30 10:48:14 -0700

    initial commit

---
+++ src/state-effects/TypeEffects.ts
@@ -0,0 +1,47 @@
+import { EditorState, StateEffect, Transaction, StateField } from "@codemirror/state"
+
+
+export const setEnumEffect = StateEffect.define<Record<string, number>>();
+export const setMoneyEffect = StateEffect.define<Record<string, number>>();
+export const setDateEffect = StateEffect.define<Record<string, number>>();
+export const setPercentEffect = StateEffect.define<Record<string, number>>();
+export const setCheckboxEffect = StateEffect.define<Record<string, number>>();
+const resetEffect = StateEffect.define();
+
+export const setIndexEffect = StateEffect.define<Record<string, number>>();
+export const TypeEffectsField = StateField.define<Record<string, number> | null>({
+  create(state: EditorState) {
+    return null;
+  },
+
+  update(value: Record<string, number> | null, transaction: Transaction): Record<string, number> | null {
+    let newState = value;
+    for (const effect of transaction.effects) {
+      if (effect.is(setIndexEffect)) {
+        return {
+          ...(value || {}),
+          ...effect.value,
+        }
+      } 
+      else if (effect.is(setEnumEffect)) {
+        return {
+          ...(value || {}),
+          ...effect.value,
+        }
+      } 
+      else if (effect.is(setMoneyEffect)) {
+        return {
+          ...(value || {}),
+          ...effect.value,
+        }
+      } 
+      else if (effect.is(resetEffect)) {
+        newState = null;
+      }
+    }
+    
+    return newState;
+
+  },
+});
+
