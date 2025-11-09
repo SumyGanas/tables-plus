ecefb260f70775de3409492ab8a8bede762f93df
SumyGanas <aksmy.997@gmail.com>
2025-11-09 11:40:06 -0800

    remove unused file

--- src/plugin-logic/modalLogic.ts
+++
@@ -1,4 +0,0 @@
-import { App, Modal, DropdownComponent, TextComponent, ButtonComponent, Setting, MarkdownView} from 'obsidian'
-import { currencies,currencyTypes } from '../ui/view-plugins/currencyState';
-import { Badges } from '../ui/modal/EnumBadge';
-
