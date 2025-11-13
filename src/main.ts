ec55c93ad40da1f4e2aa2cf4ad9d2d68ebda8e21
SumyGanas <aksmy.997@gmail.com>
2025-11-12 20:38:13 -0800

    fix ui case to match sentence case

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Plugin, MarkdownView, App, Notice } from 'obsidian';
+import { Plugin, MarkdownView, Notice } from 'obsidian';
 import { TypesModal } from '@/src/ui/modal/modal';
 import { TypeEffectsField } from '@/src/state-effects/TypeEffects';
 import { markdown } from "@codemirror/lang-markdown";
@@ -6,8 +6,8 @@ import { tableViewPlugin } from '@/src/ui/view-plugins/tableView';
 import { findIndex } from '@/src/plugin-logic/tableLogic';
 import { currencyField } from './ui/view-plugins/currencyState';
 import { tableConfigStateField } from './state-effects/enumEffects';
-import { getTableId } from './plugin-logic/modalConfigSettings';
-import {  placeholders, createPlaceholderPostProcessor } from './ui/view-plugins/enumButtons';
+import { Table } from './plugin-logic/modalConfigSettings';
+//import {  placeholders, createPlaceholderPostProcessor } from './ui/view-plugins/enumButtons';
 
 
 export default class TablesPlusPlugin extends Plugin {
@@ -15,11 +15,11 @@ export default class TablesPlusPlugin extends Plugin {
     await this.loadSettings()
 
     const view = this.app.workspace.getActiveViewOfType(MarkdownView);
-    this.registerEditorExtension([tableViewPlugin, TypeEffectsField, currencyField, tableConfigStateField, placeholders]);
+    this.registerEditorExtension([tableViewPlugin, TypeEffectsField, currencyField, tableConfigStateField]);
     this.registerEditorExtension(markdown());
-    this.registerMarkdownPostProcessor(
-        createPlaceholderPostProcessor(this.app)
-      );
+    // this.registerMarkdownPostProcessor(
+    //     createPlaceholderPostProcessor(this.app)
+    //   );
     
     let selectedElement: HTMLElement | null | undefined
     
@@ -32,26 +32,24 @@ export default class TablesPlusPlugin extends Plugin {
                             const selectionContainer = range.commonAncestorContainer;
                             const babyDiv = selectionContainer.nodeType === Node.TEXT_NODE ? selectionContainer.parentElement : selectionContainer as HTMLElement;
                             if(babyDiv) {
-                                let tableID: string = ""
                                 selectedElement = babyDiv.closest('th')
                                 const selectedtext = selection.toString()
-                                const TableID = getTableId(view.editor)
+                                //const TableID = getTableId(view.editor)
                                 const table = babyDiv.closest("table")
-                                
-                                TableID.then((c)=>{
-                                    tableID = c
-                                    if (table) table.id = c
-                                    if (tableID.length > 0){
-                                        new TypesModal(this.app,view,tableID,selectedtext,(type)=>{
+                                if (table){
+                                    const tableObj = new Table(view.editor)
+                                    const config = tableObj.getTableConf()
+                                    if (config === null || config === undefined || config.tableId === null || config.tableId === undefined){
+                                        tableObj.newConfigBlock(view.editor.getCursor().line)
+                                    }                                    
+                                    new TypesModal(this.app,view,tableObj,selectedtext,(type)=>{
                                         new Notice(`Selected: ${type}`)
                                         selectedElement?.setAttribute("modified-header", `${type}`);
                                         const headerRow = selectedElement?.closest("tr")
                                         const cellIndex = findIndex(headerRow)
                                         selectedElement?.setAttribute("cell-index", `${cellIndex}`);
                                         }).open()
-                                    } 
-                                })
-
+                                }
                             }
                         }
                     } else{
@@ -69,6 +67,7 @@ export default class TablesPlusPlugin extends Plugin {
     }
 
     async onunload() {
+        //unload function for future instance creation by a setting
     }
 
     async loadSettings() {
